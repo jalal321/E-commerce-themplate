e3a8b7c5b7b78ae784a19db3ded1434f5c001e6f
jalal321 <ahsyou1@gmail.com>
2021-07-27 16:40:47 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    $('.flexslider-main').flexslider({
+      animation: "fade",
+      pauseOnHover: true,
+      responsive: true,
+    });
+
+    $('.flexslider-top-items').flexslider({
+        animation: "slide",
+        animationLoop: true,
+        slideshow: true,
+        controlNav: false,
+        itemWidth: 315,
+        itemMargin: 5,
+        pauseOnHover: true,
+      });
+       
+      // Dropdown Menu Show On Mobile  
+
+      $('.dropdown').on('click' , function(e) {
+        console.log('clicked dropdwon');
+        $(this).find('.dropdown-menu').addClass('show-dropdown-mobile')
+      })
+
+      // To Show Dopdown On Dropdown hover
+//       $('.dropdown').hover( function(e) {
+//         console.log('clicked dropdwon for backdrop');
+//         $(this).find('.dropdown-menu').css('display' , 'block')
+//       } , function(e) {
+//         console.log('clicked dropdwon for backdrop');
+//         $('.dropdown-menu').css('display' , 'none');
+//       }
+// )
+       
+      // Close btn to hide Dropdown Menu On Mobile  
+
+      $('.btn-close').on('click' , function(e) {
+        console.log('clicked close btn');
+        e.stopPropagation();
+        $(this).parent('.dropdown-menu').removeClass('show-dropdown-mobile')
+      })
+      
+      $('.navigation').scroll( function (e) {
+        console.log('out');
+
+        if (this.scrollTop > 100) {
+        console.log('in');
+
+          $('.navigation').css('position','fixed')
+          $('.navigation').css('top','0')
+        } else {
+          
+        }
+      })
+
+  // AOS.init();
+
+  });
